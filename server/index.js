4c99ad7d7076a719bdcca383faddd1e2787dc184
Herbert <herbertvargas@gmail.com>
2021-11-19 21:22:52 -0500

    server at port 5000 and CRD operations

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const mysql_conn_data = {
   database: "nodejs_53414",
   user: "root",
   password: "root",
+  multipleStatements: true,
   //   socket: "/Applications/MAMP/tmp/mysql/mysql.sock",
 };
 
@@ -23,14 +24,16 @@ const connection = mysql.createConnection(mysql_conn_data);
 
 connection.connect(function (error) {
   if (error) {
-    console.log("Error de conexion", error.stack);
+    console.log("Error in connection", error.stack);
     return;
   }
-  console.log("conectado", connection.threadId);
+  console.log("connected, connection threadID: ", connection.threadId);
 });
 
 app.get("/", (req, res) => {
-  res.send("This is express running");
+  res.json({
+    greeting: "this is /",
+  });
 });
 
 // Get all users
@@ -44,14 +47,17 @@ app.get("/usuarios", (req, res) => {
     results.forEach((element) => {
       console.log(`id: ${element.id} nombre: ${element.nombre}`);
     });
-    res.send("consulta exitosa");
+    res.json({
+      greeting: "successful query",
+    });
+    // res.send("consulta exitosa");
   });
 });
 
 // Get an user
-app.get("/usuarios", (req, res) => {
-  let sql = "SELECT * FROM  usuarios";
-  connection.query(sql, function (error, results, fields) {
+app.get("/usuarios/:id", (req, res) => {
+  let sql = "SELECT * FROM  usuarios WHERE id=?";
+  connection.query(sql, [req.params.id], function (error, results, fields) {
     if (error) {
       throw error;
     }
@@ -59,7 +65,55 @@ app.get("/usuarios", (req, res) => {
     results.forEach((element) => {
       console.log(`id: ${element.id} nombre: ${element.nombre}`);
     });
-    res.send("consulta exitosa");
+
+    res.json({
+      greeting: "successful query",
+      id: results[0].id,
+      nombre: results[0].nombre,
+      contrasegna: results[0].contrasegna,
+      isActivo: results[0].activo,
+      fecha_creacion: results[0].fecha_hora_crea,
+    });
+  });
+});
+
+// Delete an user
+app.delete("/usuarios/:id", (req, res) => {
+  let sql = "DELETE FROM  usuarios WHERE id=?";
+  connection.query(sql, [req.params.id], function (error, results, fields) {
+    if (error) {
+      throw error;
+    }
+
+    console.log(`the user with the id: ${req.params.id} was successfully deleted`);
+    res.json({
+      greeting: "successfully deleted",
+      id: req.params.id,
+    });
+  });
+});
+
+//post an user
+app.post("/usuarios", (req, res) => {
+  //   let id = [req.body.id];
+  //   let name = [req.body.nombre];
+  //   let password = [req.body.contrasegna];
+  //   let isActive = [req.body.activo];
+  //   let fecha = [req.body.fecha];
+  //   let sql2 = `VALUES (${id}, ${name}, ${password}, ${isActive}, ${fecha})`;
+  let us = req.body;
+  let sql3 = "SET @id = ?; SET @name = ?; SET @password = ?; SET @isActive = ?, SET @fecha = ?; \
+  CALL usuarioAddOrEdit(@id, @name, @password, @isActive, @fecha)";
+
+  //   let sql = "INSERT INTO `usuarios` (`id`, `nombre`, `contrasegna`, `activo`, `fecha_hora_crea`)";
+  //   sql += sql2;
+  connection.query(sql3, [us.id, us.name, us.password, us.isActive, us.fecha], function (error, results, fields) {
+    if (error) {
+      throw error;
+    }
+    res.json({
+      greeting: "successfully added",
+    });
   });
 });
 
---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+// const port = 5000;
+
+//create express app
+const app = express();
+
+//setup server port
+const port = process.env.PORT || 5000;
+
+//parse requests of content-type- application/JSON
+app.use(express.json());
+
+//define the root route
+app.get("/", (req, res) => {
+  res.json({
+    greeting: "hello world",
+  });
+});
+
+//require user routes
+const userRoutes = require("../server/config/src/routes/user.routes");
+
+// using as a middleware
+app.use("/api/v1/users", userRoutes);
+
+app.listen(port, () => console.log(`express server is running at port ${port}`));
